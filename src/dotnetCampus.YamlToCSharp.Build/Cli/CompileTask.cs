e2f3d7faf38bd8e93f903f539b32c38347029372
walterlv <xblero@gmail.com>
2020-06-15 20:11:45 +0800

    可以生成完整的 C# 代码

--- src/dotnetCampus.YamlToCSharp.Build/Cli/CompileTask.cs
+++ src/dotnetCampus.YamlToCSharp.Build/Cli/CompileTask.cs
@@ -5,6 +5,7 @@ using System.Linq;
 
 using dotnetCampus.Cli;
 using dotnetCampus.YamlToCSharp.Core;
+using dotnetCampus.YamlToCSharp.Utils;
 
 namespace dotnetCampus.YamlToCSharp.Cli
 {
@@ -23,16 +24,16 @@ namespace dotnetCampus.YamlToCSharp.Cli
         public string? WorkingDirectory { get; set; }
 
         /// <summary>
-        /// 要转换的 YAML 文件的路径，使用分号分隔。建议使用 MSBuild 来生成这样的集合，形如 @(xxx)。
-        /// </summary>
-        [Option(nameof(YamlSourceFiles))]
-        public string? YamlSourceFiles { get; set; }
-
-        /// <summary>
         /// 项目的根命名空间。
         /// </summary>
         [Option(nameof(RootNamespace))]
         public string? RootNamespace { get; set; }
+
+        /// <summary>
+        /// 要转换的 YAML 文件的路径，使用分号分隔。建议使用 MSBuild 来生成这样的集合，形如 @(xxx)。
+        /// </summary>
+        [Option(nameof(YamlSourceFiles))]
+        public string? YamlSourceFiles { get; set; }
 
         internal void Run()
         {
@@ -48,6 +49,12 @@ namespace dotnetCampus.YamlToCSharp.Cli
                 throw new ArgumentException("必须指定项目工作路径，IntermediateOutputPath。", nameof(WorkingDirectory));
             }
 
+            var rootNamespace = RootNamespace;
+            if (rootNamespace is null || string.IsNullOrWhiteSpace(rootNamespace))
+            {
+                throw new ArgumentException("必须指定项目的根命名空间，RootNamespace。", nameof(RootNamespace));
+            }
+
             var yamlFiles = YamlSourceFiles?.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
             if (yamlFiles is null || yamlFiles.Length == 0)
             {
@@ -60,12 +67,48 @@ namespace dotnetCampus.YamlToCSharp.Cli
                 ? new DirectoryInfo(workingDirectoryString)
                 : new DirectoryInfo(Path.Combine(projectDirectory.FullName, workingDirectoryString));
 
-            RunCore(workingDirectory, RootNamespace, yamlFiles.Select(x => Path.Combine(projectDirectory.FullName, x)));
+            var convertedFiles = RunCore(projectDirectory, workingDirectory, rootNamespace,
+                yamlFiles.Select(x => new FileInfo(Path.Combine(projectDirectory.FullName, x))));
+
+            File.WriteAllText(
+                Path.Combine(workingDirectory.FullName, "OutputSourceFiles.txt"),
+                string.Join(";", convertedFiles.Select(x => x.FullName)));
         }
 
-        private void RunCore(DirectoryInfo workingDirectory, string rootNamespace, IEnumerable<string> yamlFiles)
+        private IReadOnlyList<FileInfo> RunCore(
+            DirectoryInfo projectDirectory, DirectoryInfo workingDirectory,
+            string rootNamespace, IEnumerable<FileInfo> yamlFiles)
         {
+            const string methodName = "AsDictionary";
+            const string interfaceName = "IYamlCSharpDictionary";
+            var convertedFiles = new List<FileInfo>();
 
+            // 生成接口。
+            var interfaceCreator = new ShapeInterfaceCreator(
+                rootNamespace, interfaceName,
+                $"System.Collections.Generic.Dictionary<string, string> {methodName}()");
+            var interfaceFile = new FileInfo(Path.Combine(workingDirectory.FullName, $"{interfaceName}.cs"));
+            var @interface = interfaceCreator.Create();
+            File.WriteAllText(interfaceFile.FullName, @interface);
+            convertedFiles.Add(interfaceFile);
+
+            // 生成实现类。
+            var yamlFileToCSharpFile = new YamlFileToCSharpFile();
+            foreach (var yamlFile in yamlFiles)
+            {
+                var (@namespace, @class) = IdentifierHelper.MakeNamespaceAndClassName(projectDirectory, yamlFile, rootNamespace);
+                var csharpFile = new FileInfo(Path.Combine(workingDirectory.FullName, $"{@namespace}.{@class}.cs"));
+
+                yamlFileToCSharpFile.ParseToCSharpFile(yamlFile, csharpFile,
+                    @namespace,
+                    $"{RootNamespace}.{interfaceName}",
+                    className: @class,
+                    methodName: "methodName");
+
+                convertedFiles.Add(csharpFile);
+            }
+
+            return convertedFiles;
         }
     }
 }
---
+++ src/dotnetCampus.YamlToCSharp.Build/Utils/IdentifierHelper.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace dotnetCampus.YamlToCSharp.Utils
+{
+    internal static class IdentifierHelper
+    {
+        public static (string @namespace, string @class) MakeNamespaceAndClassName(DirectoryInfo projectDirectory, FileInfo yamlFile, string rootNamespace)
+        {
+            var yamlDirectory = yamlFile.Directory;
+            var relative = PathHelper.MakeRelativePath(projectDirectory.FullName, yamlDirectory.FullName);
+            var pathParts = relative.Split(new char[] { '\\', '/' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var namespaceParts = new List<string>();
+            foreach (var part in pathParts)
+            {
+                var identifier = ConvertAnyStringToIdentifier(part);
+                if (identifier.Length > 0)
+                {
+                    namespaceParts.Add(identifier);
+                }
+            }
+
+            var namespacePart = string.Join(".", namespaceParts);
+            var yamlFileName = Path.GetFileNameWithoutExtension(yamlFile.FullName);
+            var classPart = ConvertAnyStringToIdentifier(yamlFileName);
+
+            var @namespace = string.IsNullOrWhiteSpace(namespacePart)
+                ? rootNamespace
+                : $"{rootNamespace}.{namespacePart}";
+            var @class = string.IsNullOrWhiteSpace(classPart)
+                ? "UnamedYamlObject"
+                : classPart;
+
+            return (@namespace, @class);
+        }
+
+        private static string ConvertAnyStringToIdentifier(string text)
+        {
+            var containsLetter = text.Any(x => char.IsLetter(x));
+            if (!containsLetter)
+            {
+                return "";
+            }
+
+            var partBuilder = new StringBuilder();
+            foreach (var c in text)
+            {
+                if (partBuilder.Length == 0)
+                {
+                    if (char.IsLetter(c))
+                    {
+                        partBuilder.Append(c);
+                    }
+                }
+                else
+                {
+                    var last = partBuilder[partBuilder.Length - 1];
+                    if (char.IsLetterOrDigit(c))
+                    {
+                        partBuilder.Append(c);
+                    }
+                    else if (char.IsLetterOrDigit(last))
+                    {
+                        partBuilder.Append('_');
+                    }
+                }
+            }
+
+            return partBuilder.ToString();
+        }
+    }
+}
