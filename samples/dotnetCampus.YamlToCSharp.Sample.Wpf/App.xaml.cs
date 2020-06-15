650df9dbbfba512435cea0e79601df845cac0693
walterlv <xblero@gmail.com>
2020-06-15 21:40:20 +0800

    使用示例生成的语言。

--- samples/dotnetCampus.YamlToCSharp.Sample.Wpf/App.xaml.cs
+++ samples/dotnetCampus.YamlToCSharp.Sample.Wpf/App.xaml.cs
@@ -10,5 +10,20 @@ namespace dotnetCampus.YamlToCSharp
 {
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var languages = new List<IYamlCSharpDictionary>
+            {
+                new dotnetCampus.YamlToCSharp.Localizations.en_US.Main(),
+                new dotnetCampus.YamlToCSharp.Localizations.en_US.Extension(),
+                new dotnetCampus.YamlToCSharp.Localizations.zh_CN.Main(),
+                new dotnetCampus.YamlToCSharp.Localizations.zh_CN.Extension(),
+                new dotnetCampus.YamlToCSharp.Localizations.zh_TW.Main(),
+                new dotnetCampus.YamlToCSharp.Localizations.zh_TW.Extension(),
+            };
+            var dict = languages.SelectMany(x => x.AsDictionary());
+        }
     }
 }
