5542650d0c5b0cb14a6bc4010354c6686949b82d
lindexi <lindexi_gd@163.com>
2022-10-29 16:31:54 +0800

    创建源代码生成项目

---
+++ src/dotnetCampus.YamlToCSharp.Analyzers/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+
+namespace dotnetCampus.YamlToCSharp.Analyzers;
+
+[Generator(LanguageNames.CSharp)]
+public class YamlToCSharpIncrementalGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        
+    }
+}
