dcc219dd15e2a41475f43967b18ca6a8a3e6c715
lindexi <lindexi_gd@163.com>
2022-10-29 20:19:44 +0800

    加上更多测试代码

--- samples/dotnetCampus.YamlToCSharp.SourceGeneration.Sample/MainWindow.xaml.cs
+++ samples/dotnetCampus.YamlToCSharp.SourceGeneration.Sample/MainWindow.xaml.cs
@@ -23,5 +23,6 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
 
+        
     }
 }
---
+++ samples/dotnetCampus.YamlToCSharp.SourceGeneration.Sample/Zh_cn.cs
@@ -0,0 +1,7 @@
+﻿namespace dotnetCampus.Localizations.dotnetCampus_YamlToCSharp_SourceGeneration_Sample
+{
+    public partial class Zh_cn
+    {
+
+    }
+}
